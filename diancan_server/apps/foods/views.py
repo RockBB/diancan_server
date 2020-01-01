1f52cb49c6bd61f5881701addc38da17ab5c2321
rock <chenziwu@ufactory.cc>
2020-01-01 22:41:41 +0800

    finish

--- diancan_server/apps/foods/views.py
+++ diancan_server/apps/foods/views.py
@@ -18,8 +18,8 @@ from rest_framework.pagination import PageNumberPagination
 class StandardPageNumberPagination(PageNumberPagination):
     """自定义分页器"""
     page_size_query_param = 'page_size'
-    max_page_size = 10
-    page_size = 3
+    max_page_size = 12
+    page_size = 12
 
 
 from .serializers import FoodModelSerializer
